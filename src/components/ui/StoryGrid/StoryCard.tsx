a603f6017a1368f303510431d6859ff7c4d498c0
matthewsimpson <simpson.matthew.j97@gmail.com>
2026-02-26 11:12:12 +0000

    a11y(storycard): add transform to focus state

--- src/components/ui/StoryGrid/StoryCard.tsx
+++ src/components/ui/StoryGrid/StoryCard.tsx
@@ -21,25 +21,23 @@ export default function StoryCard({
   incDate = false,
 }: CardProps) {
   return (
-    <a href="">
-      <div className={styles.card}>
-        <Image
-          src={img.desktop}
-          alt=""
-          fill
-          sizes="(max-width: 639px) 100vw, (max-width: 1023px) 50vw, 25vw"
-          className={styles.card__img}
-        />
-        <div className={styles.card__mask}></div>
-        <div className={styles.card__content}>
-          {incDate ? <p className={styles.card_info}>{date}</p> : undefined}
-          <h4 className={styles.card__title}>{title}</h4>
-          <p className={styles.card_info}>by {author}</p>
-          <hr />
-          <span className={styles.card__cta} aria-hidden>
-            read story <Icons.Arrow />
-          </span>
-        </div>
+    <a href="" className={styles.card}>
+      <Image
+        src={img.desktop}
+        alt=""
+        fill
+        sizes="(max-width: 639px) 100vw, (max-width: 1023px) 50vw, 25vw"
+        className={styles.card__img}
+      />
+      <div className={styles.card__mask}></div>
+      <div className={styles.card__content}>
+        {incDate ? <p className={styles.card_info}>{date}</p> : undefined}
+        <h4 className={styles.card__title}>{title}</h4>
+        <p className={styles.card_info}>by {author}</p>
+        <hr />
+        <span className={styles.card__cta} aria-hidden>
+          read story <Icons.Arrow />
+        </span>
       </div>
     </a>
   );
